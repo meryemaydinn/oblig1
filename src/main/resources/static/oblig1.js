47cdec86b6fba52a697f8dd07d4916297607ba28
meryemaydinn <129065741+meryemaydinn@users.noreply.github.com>
2024-02-18 08:05:05 +0100

    La til html og js.

--- src/main/resources/static/oblig1.js
+++ src/main/resources/static/oblig1.js
@@ -9,35 +9,35 @@ function BillettBestilling (){
         document.getElementById('velg').nextElementSibling.style.color = 'red';
     }
     let antall = document.getElementById('antall').value;
-    if ((antall == null) || (antall == '')) {
+    if ((antall == null) || (antall === '')) {
         let ut = 'Må skrive noe inn i antall';
         document.getElementById('antall').nextElementSibling.innerHTML = ut;
         document.getElementById('antall').nextElementSibling.style.color = 'red';
     }
     let name = document.getElementById('name').value;
-    if ((name == null) || (name == '')){
+    if ((name == null) || (name === '')){
         let ut = 'Må skrive noe inn i fornavnet';
         document.getElementById('name').nextElementSibling.innerHTML = ut;
         document.getElementById('name').nextElementSibling.style.color = 'red';
     }
     let surname = document.getElementById('surname').value;
-    if ((surname == null) || (surname == '')) {
+    if ((surname == null) || (surname === '')) {
         let ut = 'Må skirve noe inn i fornavnet';
         document.getElementById('surname').nextElementSibling.innerHTML = ut;
         document.getElementById('surname').nextElementSibling.style.color = 'red';
     }
     let phone = document.getElementById('phone').value;
-    if ((phone == null) || (phone == '')) {
+    if ((phone == null) || (phone === '')) {
         let ut = 'Må skrive noe inn i telefonnr';
         document.getElementById('phone').nextElementSibling.innerHTML = ut;
         document.getElementById('phone').nextElementSibling.style.color = 'red';
     }
     let email = document.getElementById('email').value;
-    if ((email == null) || (email == '')) {
+    if ((email == null) || (email === '')) {
         let ut = 'Må skrive noe inn i epost';
         document.getElementById('email').nextElementSibling.innerHTML = ut;
         document.getElementById('email').nextElementSibling.style.color = 'red';
     }
-    
+
 
 }
