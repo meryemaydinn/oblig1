1cb620393f83cf04da9d40b357808f810c9b5750
meryemaydinn <129065741+meryemaydinn@users.noreply.github.com>
2024-02-18 08:07:17 +0100

    La til html og js.

--- src/main/resources/static/oblig1.js
+++ src/main/resources/static/oblig1.js
@@ -4,37 +4,43 @@ function BillettBestilling (){
 
   let film = document.getElementById('film').value;
     if (film == null) {
-        let ut = 'Velg en film';
+        let ut;
+        ut = 'Velg en film';
         document.getElementById('velg').nextElementSibling.innerHTML = ut;
         document.getElementById('velg').nextElementSibling.style.color = 'red';
     }
     let antall = document.getElementById('antall').value;
     if ((antall == null) || (antall === '')) {
-        let ut = 'Må skrive noe inn i antall';
+        let ut;
+        ut = 'Må skrive noe inn i antall';
         document.getElementById('antall').nextElementSibling.innerHTML = ut;
         document.getElementById('antall').nextElementSibling.style.color = 'red';
     }
     let name = document.getElementById('name').value;
     if ((name == null) || (name === '')){
-        let ut = 'Må skrive noe inn i fornavnet';
+        let ut;
+        ut = 'Må skrive noe inn i fornavnet';
         document.getElementById('name').nextElementSibling.innerHTML = ut;
         document.getElementById('name').nextElementSibling.style.color = 'red';
     }
     let surname = document.getElementById('surname').value;
     if ((surname == null) || (surname === '')) {
-        let ut = 'Må skirve noe inn i fornavnet';
+        let ut;
+        ut = 'Må skirve noe inn i fornavnet';
         document.getElementById('surname').nextElementSibling.innerHTML = ut;
         document.getElementById('surname').nextElementSibling.style.color = 'red';
     }
     let phone = document.getElementById('phone').value;
     if ((phone == null) || (phone === '')) {
-        let ut = 'Må skrive noe inn i telefonnr';
+        let ut;
+        ut = 'Må skrive noe inn i telefonnr';
         document.getElementById('phone').nextElementSibling.innerHTML = ut;
         document.getElementById('phone').nextElementSibling.style.color = 'red';
     }
     let email = document.getElementById('email').value;
     if ((email == null) || (email === '')) {
-        let ut = 'Må skrive noe inn i epost';
+        let ut;
+        ut = 'Må skrive noe inn i epost';
         document.getElementById('email').nextElementSibling.innerHTML = ut;
         document.getElementById('email').nextElementSibling.style.color = 'red';
     }
